953ccab1e0a740e16f8777ab27c6987f3502ba8a
leonardotfa <dev.leonardotfa@gmail.com>
2023-04-11 10:54:56 -0300

    first exercices

---
+++ aula12.py
@@ -0,0 +1,9 @@
+nome = 'Leonardo'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Leonardo Tfardoski')
+print(1, 'Leonardo "Tfardoski"')
+
+# Aspas duplas
+print("Leonardo Tfardoski")
+print(2, "Leonardo 'Tfardoski'")
+
+# Escape
+print("Leonardo \"Tfardoski\"")
+
+# r
+print(r"Leonardo \"Tfardoski\"")
